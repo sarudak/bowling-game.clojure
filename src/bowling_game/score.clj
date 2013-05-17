2e17ababc84d857f30bbe391bbf3025f0975c08a
Sarudak <SPaulson@paraport.com>
2013-05-17 08:29:55 -0700

    Standardized parens

--- src/bowling_game/score.clj
+++ src/bowling_game/score.clj
@@ -7,15 +7,12 @@
 
 (defn score-frame [rolls]
   (let [contributing-rolls (if (strike-or-spare? rolls) 3 2)]
-    (apply safe-sum (take contributing-rolls rolls)))
-  )
+    (apply safe-sum (take contributing-rolls rolls))))
 
 (defn next-frame [[first-roll, second-roll & remaining]]
   (if (= first-roll 10)
     (cons second-roll remaining)
-    remaining
-    )
-  )
+    remaining))
 
 (defn score-game [game]
   (loop [score 0
@@ -25,6 +22,4 @@
       score
       (recur (+ (score-frame remaining-rolls) score)
         (next-frame remaining-rolls)
-        (inc frame))
-      ))
-  )
+        (inc frame)))))

528f2632ebb1dcf8335e233a281bee16f4b23a7b
Sarudak <SPaulson@paraport.com>
2013-05-17 08:27:25 -0700

    Implementation from kata plus a little refactoring

--- src/bowling_game/score.clj
+++ src/bowling_game/score.clj
@@ -1,3 +1,30 @@
 (ns bowling-game.score)
 
-(defn score-game [game] 0)
+(defn safe-sum [& values] (apply + (filter identity values)))
+
+(defn strike-or-spare? [[first-roll second-roll]]
+  (>= (safe-sum first-roll second-roll) 10))
+
+(defn score-frame [rolls]
+  (let [contributing-rolls (if (strike-or-spare? rolls) 3 2)]
+    (apply safe-sum (take contributing-rolls rolls)))
+  )
+
+(defn next-frame [[first-roll, second-roll & remaining]]
+  (if (= first-roll 10)
+    (cons second-roll remaining)
+    remaining
+    )
+  )
+
+(defn score-game [game]
+  (loop [score 0
+         remaining-rolls game
+         frame 1]
+    (if (or (empty? remaining-rolls) (> frame 10) )
+      score
+      (recur (+ (score-frame remaining-rolls) score)
+        (next-frame remaining-rolls)
+        (inc frame))
+      ))
+  )
